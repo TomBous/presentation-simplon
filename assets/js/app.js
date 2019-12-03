e433ca83bd1083c4840594a1ddb9266d31adaf3f
TomBous <tomasbousquet@gmail.com>
2019-12-03 16:47:52 +0100

    Transformation du trombinoscope généré en JS grace à VueJS

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+    var main = new Vue({
+                        el: '#app',
+                        data: {
+                        etudiants : [
+                            
+                                { 
+                                    "prenom"  :  "Raphael", 
+                                    "nom"   :  "Aubin", 
+                                    "url_photo"      : "./assets/img/trombinoscope/raphael.jpg"
+                                },
+                                { 
+                                    "prenom"  :  "Thomas", 
+                                    "nom"   :  "Bousquet", 
+                                    "url_photo"      : "./assets/img/trombinoscope/thomas.jpg"
+                                },
+                                { 
+                                    "prenom"  :  "Massya", 
+                                    "nom"   :  "Kone", 
+                                    "url_photo"      : "./assets/img/trombinoscope/massya.jpg"
+                                },
+                                { 
+                                    "prenom"  :  "Assa", 
+                                    "nom"   :  "Cissé", 
+                                    "url_photo"      : "./assets/img/trombinoscope/assa.jpg"
+                                },
+                                { 
+                                    "prenom"  :  "Roger", 
+                                    "nom"   :  "Damien", 
+                                    "url_photo"      : "./assets/img/trombinoscope/roger.jpg"
+                                },
+                                { 
+                                    "prenom"  :  "Alexis", 
+                                    "nom"   :  "Tahakdjian", 
+                                    "url_photo"      : "./assets/img/trombinoscope/alexis.jpg"
+
+                                }
+                        ] 
+                        }
+                    })
+
+                }
